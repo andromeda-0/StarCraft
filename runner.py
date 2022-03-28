352aae53ebc9b84b347b9c47b6e826aae7a01c0c
Zongyue Zhao <zongyuez@andrew.cmu.edu>
2022-03-28 01:17:47 -0400

    tqdm

--- runner.py
+++ runner.py
@@ -1,5 +1,8 @@
 import numpy as np
 import os
+
+from tqdm import tqdm
+
 from common.rollout import RolloutWorker, CommRolloutWorker
 from agent.agent import Agents, CommAgents
 from common.replay_buffer import ReplayBuffer
@@ -16,7 +19,9 @@ class Runner:
         else:  # no communication agent
             self.agents = Agents(args)
             self.rolloutWorker = RolloutWorker(env, self.agents, args)
-        if not args.evaluate and args.alg.find('coma') == -1 and args.alg.find('central_v') == -1 and args.alg.find('reinforce') == -1:  # these 3 algorithms are on-poliy
+        if not args.evaluate and args.alg.find('coma') == -1 and args.alg.find(
+                'central_v') == -1 and args.alg.find(
+                'reinforce') == -1:  # these 3 algorithms are on-poliy
             self.buffer = ReplayBuffer(args)
         self.args = args
         self.win_rates = []
@@ -29,37 +34,42 @@ class Runner:
 
     def run(self, num):
         time_steps, train_steps, evaluate_steps = 0, 0, -1
-        while time_steps < self.args.n_steps:
-            print('Run {}, time_steps {}'.format(num, time_steps))
-            if time_steps // self.args.evaluate_cycle > evaluate_steps:
-                win_rate, episode_reward = self.evaluate()
-                # print('win_rate is ', win_rate)
-                self.win_rates.append(win_rate)
-                self.episode_rewards.append(episode_reward)
-                self.plt(num)
-                evaluate_steps += 1
-            episodes = []
-            # 收集self.args.n_episodes个episodes
-            for episode_idx in range(self.args.n_episodes):
-                episode, _, _, steps = self.rolloutWorker.generate_episode(episode_idx)
-                episodes.append(episode)
-                time_steps += steps
-                # print(_)
-            # episode的每一项都是一个(1, episode_len, n_agents, 具体维度)四维数组，下面要把所有episode的的obs拼在一起
-            episode_batch = episodes[0]
-            episodes.pop(0)
-            for episode in episodes:
-                for key in episode_batch.keys():
-                    episode_batch[key] = np.concatenate((episode_batch[key], episode[key]), axis=0)
-            if self.args.alg.find('coma') > -1 or self.args.alg.find('central_v') > -1 or self.args.alg.find('reinforce') > -1:
-                self.agents.train(episode_batch, train_steps, self.rolloutWorker.epsilon)
-                train_steps += 1
-            else:
-                self.buffer.store_episode(episode_batch)
-                for train_step in range(self.args.train_steps):
-                    mini_batch = self.buffer.sample(min(self.buffer.current_size, self.args.batch_size))
-                    self.agents.train(mini_batch, train_steps)
+        with tqdm(total=self.args.n_steps) as pbar:
+            while time_steps < self.args.n_steps:
+                print('Run {}, time_steps {}'.format(num, time_steps))
+                if time_steps // self.args.evaluate_cycle > evaluate_steps:
+                    win_rate, episode_reward = self.evaluate()
+                    # print('win_rate is ', win_rate)
+                    self.win_rates.append(win_rate)
+                    self.episode_rewards.append(episode_reward)
+                    self.plt(num)
+                    evaluate_steps += 1
+                episodes = []
+                # 收集self.args.n_episodes个episodes
+                for episode_idx in range(self.args.n_episodes):
+                    episode, _, _, steps = self.rolloutWorker.generate_episode(episode_idx)
+                    episodes.append(episode)
+                    time_steps += steps
+                    pbar.update(time_steps)
+                    # print(_)
+                # episode的每一项都是一个(1, episode_len, n_agents, 具体维度)四维数组，下面要把所有episode的的obs拼在一起
+                episode_batch = episodes[0]
+                episodes.pop(0)
+                for episode in episodes:
+                    for key in episode_batch.keys():
+                        episode_batch[key] = np.concatenate((episode_batch[key], episode[key]),
+                                                            axis=0)
+                if self.args.alg.find('coma') > -1 or self.args.alg.find(
+                        'central_v') > -1 or self.args.alg.find('reinforce') > -1:
+                    self.agents.train(episode_batch, train_steps, self.rolloutWorker.epsilon)
                     train_steps += 1
+                else:
+                    self.buffer.store_episode(episode_batch)
+                    for train_step in range(self.args.train_steps):
+                        mini_batch = self.buffer.sample(
+                            min(self.buffer.current_size, self.args.batch_size))
+                        self.agents.train(mini_batch, train_steps)
+                        train_steps += 1
         win_rate, episode_reward = self.evaluate()
         print('win_rate is ', win_rate)
         self.win_rates.append(win_rate)
@@ -70,7 +80,8 @@ class Runner:
         win_number = 0
         episode_rewards = 0
         for epoch in range(self.args.evaluate_epoch):
-            _, episode_reward, win_tag, _ = self.rolloutWorker.generate_episode(epoch, evaluate=True)
+            _, episode_reward, win_tag, _ = self.rolloutWorker.generate_episode(epoch,
+                                                                                evaluate=True)
             episode_rewards += episode_reward
             if win_tag:
                 win_number += 1
@@ -94,12 +105,3 @@ class Runner:
         np.save(self.save_path + '/win_rates_{}'.format(num), self.win_rates)
         np.save(self.save_path + '/episode_rewards_{}'.format(num), self.episode_rewards)
         plt.close()
-
-
-
-
-
-
-
-
-
