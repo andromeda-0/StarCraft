795095dc4e714f1e913d7619a6f3b15decee296c
Zongyue Zhao <zongyuez@andrew.cmu.edu>
2022-03-31 01:08:24 -0400

    tensorboard

--- main.py
+++ main.py
@@ -7,32 +7,30 @@ from runner import Runner
 warnings.filterwarnings("ignore")
 
 if __name__ == '__main__':
-    for i in range(8):
-        args = get_common_args()
-        if args.alg.find('coma') > -1:
-            args = get_coma_args(args)
-        elif args.alg.find('central_v') > -1:
-            args = get_centralv_args(args)
-        elif args.alg.find('reinforce') > -1:
-            args = get_reinforce_args(args)
-        else:
-            args = get_mixer_args(args)
-        if args.alg.find('commnet') > -1:
-            args = get_commnet_args(args)
-        if args.alg.find('g2anet') > -1:
-            args = get_g2anet_args(args)
-        env = eval(args.map + '()')
-        env_info = env.get_configurations()
-        args.n_actions = env_info["n_actions"]
-        args.n_agents = env_info["n_agents"]
-        args.state_shape = env_info["state_shape"]
-        args.obs_shape = env_info["obs_shape"]
-        args.episode_limit = env_info["episode_limit"]
-        runner = Runner(env, args)
-        if not args.evaluate:
-            runner.run(i)
-        else:
-            M1, _ = runner.evaluate()
-            print('The M1 of {} is  {}'.format(args.alg, M1))
-            break
-        env.close()
+    args = get_common_args()
+    if args.alg.find('coma') > -1:
+        args = get_coma_args(args)
+    elif args.alg.find('central_v') > -1:
+        args = get_centralv_args(args)
+    elif args.alg.find('reinforce') > -1:
+        args = get_reinforce_args(args)
+    else:
+        args = get_mixer_args(args)
+    if args.alg.find('commnet') > -1:
+        args = get_commnet_args(args)
+    if args.alg.find('g2anet') > -1:
+        args = get_g2anet_args(args)
+    env = eval(args.map + '()')
+    env_info = env.get_configurations()
+    args.n_actions = env_info["n_actions"]
+    args.n_agents = env_info["n_agents"]
+    args.state_shape = env_info["state_shape"]
+    args.obs_shape = env_info["obs_shape"]
+    args.episode_limit = env_info["episode_limit"]
+    runner = Runner(env, args)
+    if not args.evaluate:
+        runner.run(1)
+    else:
+        M1, _ = runner.evaluate()
+        print('The M1 of {} is  {}'.format(args.alg, M1))
+    env.close()
