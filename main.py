bce77417b9b267be3d23051eb73af1f0fb086420
Zongyue Zhao <zongyuez@andrew.cmu.edu>
2022-04-07 01:34:17 -0400

    float32 for replay_buffer.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
         args = get_g2anet_args(args)
     env = eval(args.map + '()')
     env_info = env.get_configurations()
-    args.n_actions = env_info["n_actions"] - 1  # forced_drop should not be used
+    args.n_actions = env_info["n_actions"]
     args.n_agents = env_info["n_agents"]
     args.state_shape = env_info["state_shape"]
     args.obs_shape = env_info["obs_shape"]
