2e80abc2dff3abc6d9a149463f535475f2a742b2
Zongyue Zhao <zongyuez@andrew.cmu.edu>
2022-04-07 01:42:59 -0400

    float32 for replay_buffer.py

--- agent/agent.py
+++ agent/agent.py
@@ -74,7 +74,7 @@ class Agents:
         if self.args.alg == 'coma' or self.args.alg == 'central_v' or self.args.alg == 'reinforce':
             action = self._choose_action_from_softmax(q_value.cpu(), avail_actions, epsilon, evaluate)
         else:
-            q_value[avail_actions == 0.0] = - float("inf")
+            q_value[avail_actions == 0] = - float("inf")
             if np.random.uniform() < epsilon:
                 action = np.random.choice(avail_actions_ind)  # action是一个整数
             else:
