30a09d182d162a846109805d977d526f7296466d
Zongyue Zhao <zongyuez@andrew.cmu.edu>
2022-04-12 23:05:45 -0400

    experiments

---
+++ experiments/exp6-at.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+tmux new -d -s coma
+tmux send-keys -t coma "cd ..; conda activate lab-torch; python main.py --alg=coma --device 7 --map YellowEnv6_Atomic_Single" Enter
+tmux new -d -s iql
+tmux send-keys -t iql "cd ..; conda activate lab-torch; python main.py --alg=iql --device 7 --map YellowEnv6_Atomic_Single" Enter
+tmux new -d -s central_v
+tmux send-keys -t central_v "cd ..; conda activate lab-torch; python main.py --alg=central_v --device 6 --map YellowEnv6_Atomic_Single" Enter
+tmux new -d -s vdn
+tmux send-keys -t vdn "cd ..; conda activate lab-torch; python main.py --alg=vdn --device 6 --map YellowEnv6_Atomic_Single" Enter
+tmux new -d -s qmix
+tmux send-keys -t qmix "cd ..; conda activate lab-torch; python main.py --alg=qmix --device 5 --map YellowEnv6_Atomic_Single" Enter
+tmux new -d -s maven
+tmux send-keys -t maven "cd ..; conda activate lab-torch; python main.py --alg=maven --device 5 --map YellowEnv6_Atomic_Single" Enter
+tmux new -d -s reinforce
+tmux send-keys -t reinforce "cd ..; conda activate lab-torch; python main.py --alg=reinforce --device 4 --map YellowEnv6_Atomic_Single" Enter
+tmux new -d -s reinforce-g2anet
+tmux send-keys -t reinforce-g2anet "cd ..; conda activate lab-torch; python main.py --alg=reinforce+g2anet --device 4 --map YellowEnv6_Atomic_Single" Enter
+tmux new -d -s reinforce-commnet
+tmux send-keys -t reinforce-commnet "cd ..; conda activate lab-torch; python main.py --alg=reinforce+commnet --device 7 --map YellowEnv6_Atomic_Single" Enter
