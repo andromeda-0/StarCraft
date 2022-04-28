ce60cd02f4ac541472061093a3f0160745c1a104
Zongyue Zhao <zongyuez@andrew.cmu.edu>
2022-04-28 00:17:45 -0400

    lr

--- experiments/exp_fo.sh
+++ experiments/exp_fo.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-tmux new -d -s FON_A1D3
-tmux send-keys -t FON_A1D3 "cd ..; conda activate lab-torch; python main.py --alg=reinforce --device 4 --map fully_observable.FON_A1D3 --run_id non_bc" Enter
-tmux new -d -s FON_A3
-tmux send-keys -t FON_A3 "cd ..; conda activate lab-torch; python main.py --alg=reinforce --device 5 --map fully_observable.FON_A3 --run_id non_bc" Enter
-tmux new -d -s FON_A1D3_Transport
-tmux send-keys -t FON_A1D3_Transport "cd ..; conda activate lab-torch; python main.py --alg=reinforce --device 6 --map fully_observable.FON_A1D3_Transport --run_id non_bc" Enter
-tmux new -d -s FON_A3_Transport
-tmux send-keys -t FON_A3_Transport "cd ..; conda activate lab-torch; python main.py --alg=reinforce --device 7 --map fully_observable.FON_A3_Transport --run_id non_bc" Enter
+tmux new -d -s FON_A1D3_1e-4
+tmux send-keys -t FON_A1D3_1e-4 "cd ..; conda activate lab-torch; python main.py --alg=reinforce --device 6 --map fully_observable.FON_A1D3 --run_id non_bc_2 --n_steps 10000000" Enter
+tmux new -d -s FON_A3_1e-4
+tmux send-keys -t FON_A3_1e-4 "cd ..; conda activate lab-torch; python main.py --alg=reinforce --device 7 --map fully_observable.FON_A3 --run_id non_bc_2 --n_steps 10000000" Enter
+tmux new -d -s FON_A1D3_Transport_1e-4
+tmux send-keys -t FON_A1D3_Transport_1e-4 "cd ..; conda activate lab-torch; python main.py --alg=reinforce --device 6 --map fully_observable.FON_A1D3_Transport --run_id non_bc_2 --n_steps 10000000" Enter
+tmux new -d -s FON_A3_Transport_1e-4
+tmux send-keys -t FON_A3_Transport_1e-4 "cd ..; conda activate lab-torch; python main.py --alg=reinforce --device 7 --map fully_observable.FON_A3_Transport --run_id non_bc_2 --n_steps 10000000" Enter
