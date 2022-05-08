fb81d19539df0227be3e91e56515c5198e9ff7dd
Zongyue Zhao <zongyuez@andrew.cmu.edu>
2022-05-08 00:30:23 -0400

    typo

--- experiments/fully_observable.sh
+++ experiments/fully_observable.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 tmux new -d -s FON_A1D3_Random_reinforce
 tmux send-keys -t FON_A1D3_Random_reinforce "cd ..; conda activate lab-torch; python main.py --alg=reinforce --device 4 --map fully_observable.FON_A1D3_Random --run_id non_bc_5.7 --n_steps 10000000" Enter
-mux new -d -s FON_A1D3_Random_vdn
+tmux new -d -s FON_A1D3_Random_vdn
 tmux send-keys -t FON_A1D3_Random_vdn "cd ..; conda activate lab-torch; python main.py --alg=vdn --device 4 --map fully_observable.FON_A1D3_Random --run_id non_bc_5.7 --n_steps 10000000" Enter
 tmux new -d -s FON_A1D3_Random_qmix
 tmux send-keys -t FON_A1D3_Random_qmix "cd ..; conda activate lab-torch; python main.py --alg=qmix --device 4 --map fully_observable.FON_A1D3_Random --run_id non_bc_5.7 --n_steps 10000000" Enter
