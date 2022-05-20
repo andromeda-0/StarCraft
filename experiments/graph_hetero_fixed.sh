bd16d6f21aad1915c03a2946693171c7883ed4f9
Zongyue Zhao <zongyuez@andrew.cmu.edu>
2022-05-20 05:53:30 -0400

    experiments

--- experiments/graph_hetero_fixed.sh
+++ experiments/graph_hetero_fixed.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 tmux new -d -s POTopLeftFixed_Heterogeneous_reinforce
-tmux send-keys -t POTopLeftFixed_Heterogeneous_reinforce "cd ..; conda activate gym_graph; python main.py --alg=reinforce --device 6 --map saturn.POTopLeftFixed_Heterogeneous --run_id 5.20 --n_steps 20000000" Enter
+tmux send-keys -t POTopLeftFixed_Heterogeneous_reinforce "cd ..; conda activate gym_graph; python main.py --alg=reinforce --device 4 --map saturn.POTopLeftFixed_Heterogeneous --run_id 5.20 --n_steps 20000000" Enter
 tmux new -d -s POTopLeftFixed_Heterogeneous_reinforce_commnet
-tmux send-keys -t POTopLeftFixed_Heterogeneous_reinforce_commnet "cd ..; conda activate gym_graph; python main.py --alg=reinforce+commnet --device 6 --map saturn.POTopLeftFixed_Heterogeneous --run_id 5.20 --n_steps 20000000" Enter
+tmux send-keys -t POTopLeftFixed_Heterogeneous_reinforce_commnet "cd ..; conda activate gym_graph; python main.py --alg=reinforce+commnet --device 5 --map saturn.POTopLeftFixed_Heterogeneous --run_id 5.20 --n_steps 20000000" Enter
 tmux new -d -s POTopLeftFixed_Heterogeneous_reinforce_g2anet
-tmux send-keys -t POTopLeftFixed_Heterogeneous_reinforce_g2anet "cd ..; conda activate gym_graph; python main.py --alg=reinforce+g2anet --device 6 --map saturn.POTopLeftFixed_Heterogeneous --run_id 5.20 --n_steps 20000000" Enter
+tmux send-keys -t POTopLeftFixed_Heterogeneous_reinforce_g2anet "cd ..; conda activate gym_graph; python main.py --alg=reinforce+g2anet --device 5 --map saturn.POTopLeftFixed_Heterogeneous --run_id 5.20 --n_steps 20000000" Enter
 tmux new -d -s POTopLeftFixed_Heterogeneous_qmix
-tmux send-keys -t POTopLeftFixed_Heterogeneous_qmix "cd ..; conda activate gym_graph; python main.py --alg=qmix --device 6 --map saturn.POTopLeftFixed_Heterogeneous --run_id 5.20 --n_steps 20000000" Enter
+tmux send-keys -t POTopLeftFixed_Heterogeneous_qmix "cd ..; conda activate gym_graph; python main.py --alg=qmix --device 5 --map saturn.POTopLeftFixed_Heterogeneous --run_id 5.20 --n_steps 20000000" Enter
 tmux new -d -s POTopLeftFixed_Heterogeneous_maven
 tmux send-keys -t POTopLeftFixed_Heterogeneous_maven "cd ..; conda activate gym_graph; python main.py --alg=maven --device 6 --map saturn.POTopLeftFixed_Heterogeneous --run_id 5.20 --n_steps 20000000" Enter
 tmux new -d -s FOTopLeftFixed_Heterogeneous_reinforce
-tmux send-keys -t FOTopLeftFixed_Heterogeneous_reinforce "cd ..; conda activate gym_graph; python main.py --alg=reinforce --device 7 --map saturn.FOTopLeftFixed_Heterogeneous --run_id 5.20 --n_steps 20000000" Enter
+tmux send-keys -t FOTopLeftFixed_Heterogeneous_reinforce "cd ..; conda activate gym_graph; python main.py --alg=reinforce --device 6 --map saturn.FOTopLeftFixed_Heterogeneous --run_id 5.20 --n_steps 20000000" Enter
 tmux new -d -s FOTopLeftFixed_Heterogeneous_reinforce_commnet
-tmux send-keys -t FOTopLeftFixed_Heterogeneous_reinforce_commnet "cd ..; conda activate gym_graph; python main.py --alg=reinforce+commnet --device 7 --map saturn.FOTopLeftFixed_Heterogeneous --run_id 5.20 --n_steps 20000000" Enter
+tmux send-keys -t FOTopLeftFixed_Heterogeneous_reinforce_commnet "cd ..; conda activate gym_graph; python main.py --alg=reinforce+commnet --device 6 --map saturn.FOTopLeftFixed_Heterogeneous --run_id 5.20 --n_steps 20000000" Enter
 tmux new -d -s FOTopLeftFixed_Heterogeneous_reinforce_g2anet
 tmux send-keys -t FOTopLeftFixed_Heterogeneous_reinforce_g2anet "cd ..; conda activate gym_graph; python main.py --alg=reinforce+g2anet --device 7 --map saturn.FOTopLeftFixed_Heterogeneous --run_id 5.20 --n_steps 20000000" Enter
 tmux new -d -s FOTopLeftFixed_Heterogeneous_qmix
