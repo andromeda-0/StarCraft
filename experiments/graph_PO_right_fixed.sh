c921f78abdc11766d1b8d10c252b7698a02e0c31
Zongyue Zhao <zongyuez@andrew.cmu.edu>
2022-05-21 18:19:23 -0400

    experiments

---
+++ experiments/graph_PO_right_fixed.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+tmux new -d -s PORightFixed_Heterogeneous_reinforce
+tmux send-keys -t PORightFixed_Heterogeneous_reinforce "cd ..; conda activate gym_graph; python main.py --alg=reinforce --device 4 --map saturn.PORightFixed_Heterogeneous --run_id 5.21 --n_steps 20000000" Enter
+tmux new -d -s PORightFixed_Heterogeneous_reinforce_commnet
+tmux send-keys -t PORightFixed_Heterogeneous_reinforce_commnet "cd ..; conda activate gym_graph; python main.py --alg=reinforce+commnet --device 4 --map saturn.PORightFixed_Heterogeneous --run_id 5.21 --n_steps 20000000" Enter
+tmux new -d -s PORightFixed_Heterogeneous_reinforce_g2anet
+tmux send-keys -t PORightFixed_Heterogeneous_reinforce_g2anet "cd ..; conda activate gym_graph; python main.py --alg=reinforce+g2anet --device 5 --map saturn.PORightFixed_Heterogeneous --run_id 5.21 --n_steps 20000000" Enter
+tmux new -d -s PORightFixed_Heterogeneous_qmix
+tmux send-keys -t PORightFixed_Heterogeneous_qmix "cd ..; conda activate gym_graph; python main.py --alg=qmix --device 5 --map saturn.PORightFixed_Heterogeneous --run_id 5.21 --n_steps 20000000" Enter
+tmux new -d -s PORightFixed_Heterogeneous_maven
+tmux send-keys -t PORightFixed_Heterogeneous_maven "cd ..; conda activate gym_graph; python main.py --alg=maven --device 5 --map saturn.PORightFixed_Heterogeneous --run_id 5.21 --n_steps 20000000" Enter
