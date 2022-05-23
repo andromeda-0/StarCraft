b53c2fecd56c88d3be8ad092e475a7fe94f7df38
Zongyue Zhao <zongyuez@andrew.cmu.edu>
2022-05-22 22:45:06 -0400

    experiments

---
+++ experiments/graph_PO_full_fixed.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+tmux new -d -s POFullFixed_Heterogeneous_reinforce
+tmux send-keys -t POFullFixed_Heterogeneous_reinforce "cd ..; conda activate gym_graph; python main.py --alg=reinforce --device 6 --map saturn.POFullFixed_Heterogeneous --run_id 5.21 --n_steps 20000000" Enter
+tmux new -d -s POFullFixed_Heterogeneous_reinforce_g2anet
+tmux send-keys -t POFullFixed_Heterogeneous_reinforce_g2anet "cd ..; conda activate gym_graph; python main.py --alg=reinforce+g2anet --device 6 --map saturn.POFullFixed_Heterogeneous --run_id 5.21 --n_steps 20000000" Enter
+tmux new -d -s POFullFixed_Heterogeneous_qmix
+tmux send-keys -t POFullFixed_Heterogeneous_qmix "cd ..; conda activate gym_graph; python main.py --alg=qmix --device 7 --map saturn.POFullFixed_Heterogeneous --run_id 5.21 --n_steps 20000000" Enter
+tmux new -d -s POFullFixed_Heterogeneous_maven
+tmux send-keys -t POFullFixed_Heterogeneous_maven "cd ..; conda activate gym_graph; python main.py --alg=maven --device 7 --map saturn.POFullFixed_Heterogeneous --run_id 5.21 --n_steps 20000000" Enter
