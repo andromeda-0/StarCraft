9effcbd54b3a9250f30868a9f0f614064e95b6dd
Zongyue Zhao <zongyuez@andrew.cmu.edu>
2022-07-14 09:02:18 -0400

    final benchmark experiments

---
+++ experiments/graph_PO_right_7.14.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+tmux new -d -s PORight_Heterogeneous_reinforce
+tmux send-keys -t PORight_Heterogeneous_reinforce "cd ..; conda activate gym_graph; python main.py --alg=reinforce --device 3 --map saturn.PORight_Heterogeneous --run_id 7.14 --n_steps 200000000 --gamma 0.7" Enter
+tmux new -d -s PORight_Heterogeneous_reinforce_commnet
+tmux send-keys -t PORight_Heterogeneous_reinforce_commnet "cd ..; conda activate gym_graph; python main.py --alg=reinforce+commnet --device 4 --map saturn.PORight_Heterogeneous --run_id 7.14 --n_steps 200000000 --gamma 0.7" Enter
+tmux new -d -s PORight_Heterogeneous_reinforce_g2anet
+tmux send-keys -t PORight_Heterogeneous_reinforce_g2anet "cd ..; conda activate gym_graph; python main.py --alg=reinforce+g2anet --device 5 --map saturn.PORight_Heterogeneous --run_id 7.14 --n_steps 200000000 --gamma 0.7" Enter
+tmux new -d -s PORight_Heterogeneous_qmix
+tmux send-keys -t PORight_Heterogeneous_qmix "cd ..; conda activate gym_graph; python main.py --alg=qmix --device 6 --map saturn.PORight_Heterogeneous --run_id 7.14 --n_steps 200000000 --gamma 0.7" Enter
+tmux new -d -s PORight_Heterogeneous_maven
+tmux send-keys -t PORight_Heterogeneous_maven "cd ..; conda activate gym_graph; python main.py --alg=maven --device 7 --map saturn.PORight_Heterogeneous --run_id 7.14 --n_steps 200000000 --gamma 0.7" Enter
---
+++ experiments/graph_PO_right_cat_7.14.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+tmux new -d -s PORight_Heterogeneous_reinforce_cat
+tmux send-keys -t PORight_Heterogeneous_reinforce_cat "cd ..; conda activate gym_graph; python main.py --alg=reinforce --device 4--map saturn.PORight_Heterogeneous --run_id 7.14-cat --n_steps 200000000 --cat_state --gamma 0.7" Enter
+tmux new -d -s PORight_Heterogeneous_reinforce_commnet_cat
+tmux send-keys -t PORight_Heterogeneous_reinforce_commnet_cat "cd ..; conda activate gym_graph; python main.py --alg=reinforce+commnet --device 3 --map saturn.PORight_Heterogeneous --run_id 7.14-cat --n_steps 200000000 --cat_state --gamma 0.7" Enter
+tmux new -d -s PORight_Heterogeneous_reinforce_g2anet_cat
+tmux send-keys -t PORight_Heterogeneous_reinforce_g2anet_cat "cd ..; conda activate gym_graph; python main.py --alg=reinforce+g2anet --device 2 --map saturn.PORight_Heterogeneous --run_id 7.14-cat --n_steps 200000000 --cat_state --gamma 0.7" Enter
+tmux new -d -s PORight_Heterogeneous_qmix_cat
+tmux send-keys -t PORight_Heterogeneous_qmix_cat "cd ..; conda activate gym_graph; python main.py --alg=qmix --device 1 --map saturn.PORight_Heterogeneous --run_id 7.14-cat --n_steps 200000000 --cat_state --gamma 0.7" Enter
+tmux new -d -s PORight_Heterogeneous_maven_cat
+tmux send-keys -t PORight_Heterogeneous_maven_cat "cd ..; conda activate gym_graph; python main.py --alg=maven --device 0 --map saturn.PORight_Heterogeneous --run_id 7.14-cat --n_steps 200000000 --cat_state --gamma 0.7" Enter
