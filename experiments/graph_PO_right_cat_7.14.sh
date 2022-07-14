fb4f9813f4b7dc2773d2ff22591752b643cba27e
Zongyue Zhao <zongyuez@andrew.cmu.edu>
2022-07-14 09:08:08 -0400

    final benchmark experiments

--- experiments/graph_PO_right_cat_7.14.sh
+++ experiments/graph_PO_right_cat_7.14.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 tmux new -d -s PORight_Heterogeneous_reinforce_cat
-tmux send-keys -t PORight_Heterogeneous_reinforce_cat "cd ..; conda activate gym_graph; python main.py --alg=reinforce --device 4--map saturn.PORight_Heterogeneous --run_id 7.14-cat --n_steps 200000000 --cat_state --gamma 0.7" Enter
+tmux send-keys -t PORight_Heterogeneous_reinforce_cat "cd ..; conda activate gym_graph; python main.py --alg=reinforce --device 4 --map saturn.PORight_Heterogeneous --run_id 7.14-cat --n_steps 200000000 --cat_state --gamma 0.7" Enter
 tmux new -d -s PORight_Heterogeneous_reinforce_commnet_cat
 tmux send-keys -t PORight_Heterogeneous_reinforce_commnet_cat "cd ..; conda activate gym_graph; python main.py --alg=reinforce+commnet --device 3 --map saturn.PORight_Heterogeneous --run_id 7.14-cat --n_steps 200000000 --cat_state --gamma 0.7" Enter
 tmux new -d -s PORight_Heterogeneous_reinforce_g2anet_cat
